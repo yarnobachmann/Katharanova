277ee72c55e2c345cd4d100d4d010c0b8a0133fa
yarnobachmann <minejarno@gmail.com>
2026-07-08 18:36:19 +0200

    Fix favicon metadata

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,16 +12,15 @@ export default function manifest(): MetadataRoute.Manifest {
     lang: 'nl',
     icons: [
       {
-        src: '/icon.png',
-        sizes: '512x512',
+        src: '/icon-192.png',
+        sizes: '192x192',
         type: 'image/png'
       },
       {
-        src: '/assets/logo-phoenix-mark.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png'
       }
     ]
   }
 }
-
--- lib/seo.ts
+++ lib/seo.ts
@@ -35,6 +35,16 @@ export function createMetadata({
     metadataBase: new URL(serverUrl),
     title,
     description,
+    icons: {
+      icon: [
+        { url: '/favicon.ico', sizes: 'any' },
+        { url: '/icon-192.png', type: 'image/png', sizes: '192x192' },
+        { url: '/icon-512.png', type: 'image/png', sizes: '512x512' }
+      ],
+      apple: [
+        { url: '/apple-touch-icon.png', type: 'image/png', sizes: '180x180' }
+      ]
+    },
     alternates: {
       canonical
     },
